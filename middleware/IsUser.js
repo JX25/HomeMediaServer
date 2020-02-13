bfc229c47a5af56cad601a800c9d5b3bf95482f8
Jakub <jeehox25@gmail.com>
2020-02-13 17:10:53 +0100

    Created middleware for admin root

--- middleware/IsUser.js
+++ middleware/IsUser.js
@@ -1,11 +1,18 @@
 const middlewareUtil = require('../utils/middlewareUtil');
 
-module.exports = (req, res) => {
+module.exports = (req, res, nick) => {
     try{
         req.userData = middlewareUtil.decodeToken(req);
+        if(nick){
+            if(req.userData.nickname != nick){
+                return res.status(400).json({
+                    info: "You are trying to gain access to not your account"
+                })
+            }
+        }
     } catch(error){
         return res.status(401).json({
             info: "Authorization failed. Please login"
         })
     }
-}
+};
